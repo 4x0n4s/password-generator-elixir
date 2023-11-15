8e2ffada681f546aaf63a76e754835db74d91052
Αξονας <128817936+4x0n4s@users.noreply.github.com>
2023-11-15 19:40:30 +0100

    Create password_generator.exs

---
+++ password_generator.exs
@@ -0,0 +1,44 @@
+defmodule PasswordGenerator do
+  @alphanumeric "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  @special "!@#^&*();?:"
+  def generate(length, special \\ false) when is_integer(length) and length > 0 do
+      characters = @alphanumeric <> if special, do: @special, else: ""
+      password =
+        for _ <- 1..length, do: Enum.random(String.codepoints(characters))
+        Enum.join(password, "")
+  end
+end
+
+defmodule PasswordManager do
+  @filename "passwords.txt" #File where the last password is saved
+
+  def run do
+    IO.puts("How many characters should your password have?")
+    number = String.to_integer(String.trim(IO.gets("")))
+
+    IO.puts("Include special characters? (yes/no)")
+    special = String.trim(IO.gets(""))
+
+  result =
+    case special do
+      "yes" -> PasswordGenerator.generate(number, true)
+      "no" -> PasswordGenerator.generate(number, false)
+      _ -> IO.puts("\e[H\e[2J")
+      _ -> "Error, please respect the syntax... (yes/no)"
+      _ -> PasswordManager.run()
+    end
+
+  IO.puts("--------------------\n      #{result}    \n--------------------")
+
+  IO.puts("Do you want to save it? (yes/no)")
+  case IO.gets("") |> String.trim do
+    "yes" -> File.write!(@filename, result)
+      IO.puts("Registration in #{@filename}..")
+    "no" -> :ok
+    _ -> IO.puts("Password not saved, please respect the syntax... (yes/no)")
+  end
+
+  end
+end
+
+PasswordManager.run()
