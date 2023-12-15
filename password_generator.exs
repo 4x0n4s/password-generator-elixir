3129b945d3c8644480c29eb6ba8433e2ddfbcf04
Αξονας <128817936+4x0n4s@users.noreply.github.com>
2023-12-15 09:51:20 +0100

    Update password_generator.exs

--- password_generator.exs
+++ password_generator.exs
@@ -1,18 +1,15 @@
 defmodule PasswordGenerator do
+  @filename "passwords.txt" #File where the last password is saved
   @alphanumeric "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   @special "!@#^&*();?:"
   
   def generate(length, special \\ false) when is_integer(length) and length > 0 do
-      characters = @alphanumeric <> if special, do: @special, else: ""
-      password =
-        for _ <- 1..length, do: Enum.random(String.codepoints(characters))
-        Enum.join(password, "")
+    characters = @alphanumeric <> if special, do: @special, else: ""
+    password =
+      for _ <- 1..length, do: Enum.random(String.codepoints(characters))
+      Enum.join(password, "")
   end
-end
-
-defmodule PasswordManager do
-  @filename "passwords.txt" #File where the last password is saved
-
+  
   def run do
     IO.puts("How many characters should your password have?")
     number = String.to_integer(String.trim(IO.gets("")))
@@ -26,10 +23,10 @@ defmodule PasswordManager do
         "no" -> PasswordGenerator.generate(number, false)
         _ -> IO.puts("\e[H\e[2J")
           IO.puts("Error, please respect the syntax... (yes/no)")
-          PasswordManager.run()
+          PasswordGenerator.run()
       end
 
-    IO.puts("--------------------\n      #{result}    \n--------------------")
+    IO.puts("--------------------\n#{result}\n--------------------")
 
     IO.puts("Do you want to save it? (yes/no)")
     case IO.gets("") |> String.trim do
@@ -45,4 +42,4 @@ defmodule PasswordManager do
   end
 end
 
-PasswordManager.run()
+PasswordGenerator.run()
